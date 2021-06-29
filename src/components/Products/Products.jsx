cbedf527b21829d2489f4f18945c7747e9b20967
Taylor Sanders <tsanders3515@gmail.com>
2021-06-28 17:54:58 -0800

    products to render + title, description, image

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,9 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'French Macarons', description: 'Pink Macarons', price: '$15', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/easy-french-macarons-vertical-1545085468.jpg' },
-    { id: 2, name: 'Rainbow Cake', description: 'Rainbow Cake', price: '$60', image: 'https://sugargeekshow.com/wp-content/uploads/2020/03/rainbow-cake-featured-scaled.jpg' },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
+
     return (
         <main className={classes.content}>
             <div className={classes.toolbar} />
