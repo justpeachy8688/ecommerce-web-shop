3f49aecddcd2811963e4ac4294942cd0e8df0d2e
Taylor Sanders <tsanders3515@gmail.com>
2021-07-14 15:41:42 -0800

    address form

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
         <Typography variant="subtitle1">You have no items in your shopping cart,
-            <Link to="/" className={classes.link}>start adding some</Link>!
+            <Link to="/" className={classes.link}> start adding some</Link>!
         </Typography>
     );
 
@@ -19,7 +19,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
                     </Grid>
                 ))}
             </Grid>
@@ -28,8 +28,8 @@ const Cart = ({ cart }) => {
                     Subtotal: {cart.subtotal.formatted_with_symbol}
                 </Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
-                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
+                    <Button component={Link} to="/checkout"className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
                 </div>
             </div>
         </>
